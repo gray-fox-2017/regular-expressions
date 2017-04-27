d6a78fd7e49a40ba5027be505892495566ed338d
hakiemaul <noersedya@gmail.com>
2017-04-27 11:39:04 +0700

    fixed and refactored

--- regular_expressions.js
+++ regular_expressions.js
@@ -50,12 +50,9 @@ console.log(grab_all_nomor_ktp('please confirm your identity: XXX-XX-1422')) // 
 
 // Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
 const hide_all_nomor_ktp = (string) => {
-  let ktp=string.match(/\d{3}-\d{2}-\d{4}/g);
+  let ktp=string.replace(/(\d{3})-(\d{2})-(\d{4})/g, "XXX-XX-$3");
   if(ktp!==null) {
-    for(let i=0;i<ktp.length;i++) {
-      ktp[i]='XXX-XX-'+ktp[i].match(/\d{4}/g)
-    }
-    return ktp.join(', ');
+    return ktp;
   } else {
     return string;
   }
@@ -74,37 +71,35 @@ console.log(hide_all_nomor_ktp(hideString) === hideString) // true
 // Ensure all of the Social Security numbers use dashes for delimiters.
 // Example: 480.01.4430 and 480014430 would both be 480-01-4430.
 const format_nomor = (string) => {
-  let arr=string.split(' ');
   let ktp=[];
   let hitungKtp=0;
-  for(let i=0;i<arr.length;i++) {
-    ktp[i]=arr[i].match(/\d{3}.?\d{2}.?\d{4}/g);
-    if(ktp[i]!==null) {
-      ktp[i]=ktp[i].toString();
-      if(ktp[i].length<11) {
-        let formatKtp=ktp[i].split('');
-        formatKtp.splice(3,0,'-');
-        formatKtp.splice(6,0,'-');
-        ktp[i]=formatKtp.join('');
-      } else if(ktp[i].length==11) {
-        let formatKtp=ktp[i].split('');
-        formatKtp[3]='-';
-        formatKtp[6]='-';
-        ktp[i]=formatKtp.join('');
-      }
-    }
-    if(ktp[i]!==null) hitungKtp++;
-  }
-  if(hitungKtp>0) {
-    let newArray=[];
-    for(let i = 0; i < ktp.length; i++) {
-      if (ktp[i]) {
-        newArray.push(ktp[i]);
-      }
-    }
-    return newArray.join(', ');
-  }
-  return arr.join(' ');
+  ktp = string.replace(/(\d{3}).?(\d{2}).?(\d{4})/g, "$1-$2-$3");
+  return ktp;
+  // if(ktp[i]!==null) {
+  //   ktp[i]=ktp[i].toString();
+  //   if(ktp[i].length<11) {
+  //     let formatKtp=ktp[i].split('');
+  //     formatKtp.splice(3,0,'-');
+  //     formatKtp.splice(6,0,'-');
+  //     ktp[i]=formatKtp.join('');
+  //   } else if(ktp[i].length==11) {
+  //     let formatKtp=ktp[i].split('');
+  //     formatKtp[3]='-';
+  //     formatKtp[6]='-';
+  //     ktp[i]=formatKtp.join('');
+  //   }
+  // }
+  // if(ktp[i]!==null) hitungKtp++;
+  // if(hitungKtp>0) {
+  //   let newArray=[];
+  //   for(let i = 0; i < ktp.length; i++) {
+  //     if (ktp[i]) {
+  //       newArray.push(ktp[i]);
+  //     }
+  //   }
+  //   return newArray.join(', ');
+  // }
+  // return arr.join(' ');
 }
 
 console.log(format_nomor('234601422, 350.80.0744, 013-60-8762'))
